75a531e1bc589e3af55c2f79a4b3318e7c02b9b2
HayatoAoi <aoi.8810.hyt@gmail.com>
2024-10-12 16:04:22 +0900

    make fmt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 use anyhow::Result;
 use axum::{extract::State, http::StatusCode, routing::get, Router};
-use tokio::net::TcpListener;
 use sqlx::{postgres::PgConnectOptions, PgPool};
+use tokio::net::TcpListener;
 
 struct DatabaseConfig {
     pub host: String,
@@ -14,12 +14,12 @@ struct DatabaseConfig {
 }
 impl From<DatabaseConfig> for PgConnectOptions {
     fn from(cfg: DatabaseConfig) -> Self {
-	Self::new()
-	    .host(&cfg.host)
-	    .port(cfg.port)
-	    .username(&cfg.username)
-	    .password(&cfg.password)
-	    .database(&cfg.database)
+        Self::new()
+            .host(&cfg.host)
+            .port(cfg.port)
+            .username(&cfg.username)
+            .password(&cfg.password)
+            .database(&cfg.database)
     }
 }
 
@@ -34,26 +34,26 @@ async fn health_check() -> StatusCode {
 async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
     let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
     match connection_result {
-	Ok(_)  => StatusCode::OK,
-	Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
     let database_cfg = DatabaseConfig {
-	host: "localhost".into(),
-	port: 5432,
-	username: "app".into(),
-	password: "passwd".into(),
-	database: "app".into(),
+        host: "localhost".into(),
+        port: 5432,
+        username: "app".into(),
+        password: "passwd".into(),
+        database: "app".into(),
     };
     let conn_pool = connect_database_with(database_cfg);
-    
+
     let app = Router::new()
-	.route("/health", get(health_check))
-	.route("/health/db", get(health_check_db))
-	.with_state(conn_pool);
+        .route("/health", get(health_check))
+        .route("/health/db", get(health_check_db))
+        .with_state(conn_pool);
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await.unwrap();
 
